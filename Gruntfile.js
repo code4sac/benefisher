3d35217416f593b977620ab65b9c5ad07068af4f
Jesse Rosato <jesse.rosato@gmail.com>
2014-09-20 13:42:01 -0700

    Added gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    express: {
+      dev: {
+        options: {
+          port: 3000,
+          script: 'bin/www'
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'public/stylesheets/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss', //
+        tasks: ['sass']
+      },
+      express: {
+        files:  [ 'app.js', 'routes/*.js' ],
+        tasks:  [ 'express:dev' ],
+        options: {
+          spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-npm-install');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('build-dev', ['npm-install']);
+  grunt.registerTask('dev', ['express:dev', 'watch']);
+}
