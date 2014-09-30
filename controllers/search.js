6b9213a936e8bb8df182ea4d10112f16791dea9d
Jesse Rosato <jesse.rosato@gmail.com>
2014-09-29 20:53:50 -0700

    Search controller now accepts 'bounds' parameter (e.g. /search?bounds=maxlat,minlong,minlat,maxlong).

--- controllers/search.js
+++ controllers/search.js
@@ -12,6 +12,8 @@ var SearchController = function(req, res, fileSystem)
 {
 
   var terms = constructQueryTermsArray(req.query);
+  // Bounds query string should look like: 'top,left,bottom,right'.
+  var bounds = constructQueryBounds(req.query);
 
   /**
    * Filter all the results based on query parameters, and render the results.
@@ -25,8 +27,18 @@ var SearchController = function(req, res, fileSystem)
         res.status(500).send("Error loading data.");
       } else {
         var result = JSON.parse(data);
+        if (bounds) {
+          var i = result.length;
+          while (i--) {
+            result[i].locations = result[i].locations.filter(isInQueryBounds);
+            // If there's no locations, remove the result.
+            if ( ! result[i].locations.length) {
+              result.splice(i, 1);
+            }
+          }
+        }
         if ( terms.length ) {
-          result = result.filter(filterByQueryTerms);
+          result = result.filter(containsQueryTerms);
         }
         res.json(result);
       }
@@ -50,28 +62,68 @@ var SearchController = function(req, res, fileSystem)
   }
 
   /**
+   * Build a coordinates object from query parameters.
+   * @param query
+   * @returns {boolean|Object}
+   */
+  function constructQueryBounds(query)
+  {
+    if (query && query.bounds) {
+      var boundsArray = query.bounds.split(',');
+      if (boundsArray.length == 4) {
+        bounds = {
+          top: parseFloat(boundsArray[0]),
+          left: parseFloat(boundsArray[1]),
+          bottom: parseFloat(boundsArray[2]),
+          right: parseFloat(boundsArray[3])
+        };
+      }
+    }
+    return bounds;
+  }
+
+  /**
+   * Determine whether a location is within the query bounds.
+   * @param location
+   * @returns {boolean}
+   */
+  function isInQueryBounds(location)
+  {
+    if ( ! location.latitude || ! location.longitude) {
+      return false;
+    }
+    console.log(bounds);
+    console.log(location.latitude);
+    console.log(location.longitude);
+    return location.latitude <= bounds.top
+      && location.longitude >= bounds.left
+      && location.latitude >= bounds.bottom
+      && location.longitude <= bounds.right;
+  }
+
+  /**
    * Recursive function to filter an item by search terms.
    * @param item
    */
-  function filterByQueryTerms(item)
+  function containsQueryTerms(item)
   {
     // Recursive case: item is array.
     if (Object.prototype.toString.call(item) === '[object Array]') {
       // If there are any objects left in the filtered array, query terms were found.
-      return item.filter(filterByQueryTerms).length;
+      return item.filter(containsQueryTerms).length;
 
     }
     // Recursive case: item is object.
     if (typeof item === 'object') {
       for (property in item) {
-        if (filterByQueryTerms(item[property])) {
+        if (containsQueryTerms(item[property])) {
           return true;
         }
       }
     }
     // Terminating case: item is string.
     if (typeof item === 'string') {
-      for (i = 0; i < terms.length; i++) {
+      for (var i = 0; i < terms.length; i++) {
         if (item.match(terms[i])) {
           return true;
         }
