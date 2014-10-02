bb5b67fe0e4483800348215d92a55d418b18fb81
Jesse Rosato <jesse.rosato@gmail.com>
2014-10-02 11:48:44 -0700

    Added search service to map controller.

--- public/javascripts/controllers/map-controller.js
+++ public/javascripts/controllers/map-controller.js
@@ -3,111 +3,141 @@
  */
 
 /**
- *
+ * Map Controller
  * @param $scope
  * @param $http
  * @param search
+ * @param leafletData
  * @constructor
  */
-var MapController = function($scope, $http, search) {
+var MapController = function($scope, search, leafletData) {
 
-    // The ID of the Mapbox project to use for map tiles.
-    var MAP_ID = 'jesserosato.jihh0bm7';
-    // The map tiles URL.
-    var TILE_URL = "https://{s}.tiles.mapbox.com/v3/" + MAP_ID + "/{z}/{x}/{y}.png";
-    // The default, maximum and minimum zoom levels for the map
-    var ZOOM_DEFAULT = 13;
-    var ZOOM_MAX = 18;
-    var ZOOM_MIN = 10;
-    // The position for map controls: topright | topleft | bottomright | bottomleft
-    var CONTROLS_POSITION = 'bottomright';
+  /** CONSTANTS **/
+  // The ID of the Mapbox project to use for map tiles.
+  var MAP_ID = 'jesserosato.jihh0bm7';
+  // The map tiles URL.
+  var TILE_URL = "https://{s}.tiles.mapbox.com/v3/" + MAP_ID + "/{z}/{x}/{y}.png";
+  // The default, maximum and minimum zoom levels for the map
+  var ZOOM_DEFAULT = 13;
+  var ZOOM_MAX = 18;
+  var ZOOM_MIN = 10;
+  // The position for map controls: topright | topleft | bottomright | bottomleft
+  var CONTROLS_POSITION = 'bottomright';
 
-    // CONTROLLER LOGIC
-    initMap();
-    // TODO Adrian: Used to test adding markers. Should be removed.
-    displayMarkers();
-    search.search({});
+  /** PROPERITES **/
+  // The current and previous bounds.
+  var curBounds;
+  var prevBounds;
 
-    /**
-     * Initialize the map.
-     */
-    function initMap() {
-        // Extend scope object with defaults and center for map.
-        angular.extend($scope, {
-            defaults: {
-                tileLayer: TILE_URL,
-                maxZoom: ZOOM_MAX,
-                minZoom: ZOOM_MIN,
-                zoomControlPosition: CONTROLS_POSITION,
-                tileLayerOptions: {
-                    detectRetina: true,
-                    reuseTiles: true
-                }
-            },
-            // Use Sacramento as default center
-            center: {
-                lat: 38.5556,
-                lng: -121.4689,
-                zoom: ZOOM_DEFAULT
-            },
+  /** CONSTRUCTOR LOGIC **/
+  initMap();
+  // Listen for map movement
+  $scope.$on('leafletDirectiveMap.moveend', function(event){
+    leafletData.getMap().then(function(map) {
+      if (boundsHaveChanged(map)) {
+        search.search({ bounds: getBounds() });
+      }
+    });
+  });
 
-            markers: [],
+  /** METHODS **/
 
-            results: []
-        });
+  /**
+   * Receive a promise that results are coming from the database.
+   * @param data
+   */
+  this.update = function(data) {
+    updateMarkers(data);
+  };
 
+  /**
+   * Initialize the map.
+   */
+  function initMap() {
+    var defaults = {
+      tileLayer: TILE_URL,
+      maxZoom: ZOOM_MAX,
+      minZoom: ZOOM_MIN,
+      zoomControlPosition: CONTROLS_POSITION,
+      tileLayerOptions: {
+        detectRetina: true,
+        reuseTiles: true
+      }
     };
-
-    /**
-     * Identify locations returned through user search by adding markers on the map.
-     */
-    function displayMarkers() {
-        $http.get('/search').success(showMarkers).error(showError);
-
-        function showMarkers(data) {
-            // Remove all the previous markers before adding new ones.
-            removeMarkers();
-            data.forEach(function (service) {
-                addMarker(service);
-            });
-        }
-
-        function showError(data, status, headers, config) {
-            // TODO Adrian: What should be done if there is an error?
-        }
-
+    // Use Sacramento as default center
+    var center = {
+      lat: 38.5556,
+      lng: -121.4689,
+      zoom: ZOOM_DEFAULT
     };
+    // Extend scope object with defaults and center for map.
+    angular.extend($scope, { defaults: defaults, center: center, markers: [], results: [] });
+  }
 
-    /**
-     * Adds a marker to the map with the attributes needed.
-     * @param service - The service object to add to the map.
-     */
-    function addMarker(service) {
-
-        var name = service.name;
-        var lat = service.locations[0].latitude;
-        var lng = service.locations[0].longitude;
-        var desc = service.locations[0].description;
-        var city = service.locations[0].address_attributes.city;
-        var state = service.locations[0].address_attributes.state;
-        var street = service.locations[0].address_attributes.street;
-        var zip = service.locations[0].address_attributes.zip;
-        var hours = service.locations[0].hours;
-        var urls = service.locations[0].urls;
-
-        var message = '<h4>' + name + '<h4>\n<h5>' + desc + '</h5>\n<h5>' + hours + '</h5>';
-
-        $scope.markers.push({
-            lat: lat,
-            lng: lng,
-            message: message
-        });
-    };
-
-    /**
-     * Removes all markers from the array by creating a new one.
-     */
-    function removeMarkers() {
-        $scope.markers = [];
+  /**
+   * Returns true if the new bounds are not contained by the existing bounds.
+   * @param map
+   * @returns {boolean}
+   */
+  function boundsHaveChanged(map)
+  {
+    if (curBounds) {
+      prevBounds = curBounds;
     }
+    curBounds = map.getBounds();
+    return ( ! prevBounds || ! prevBounds.contains(curBounds));
+  }
+
+  /**
+   * Proxy for search service's search()
+   */
+  function getBounds()
+  {
+    var nw = curBounds.getNorthWest();
+    var se = curBounds.getSouthEast();
+    return nw.lat + ',' + nw.lng + ',' + se.lat + ',' + se.lng;
+  }
+
+  /**
+   * Identify locations returned through user search by adding markers on the map.
+   */
+  function updateMarkers(data) {
+    // Remove all the previous markers before adding new ones.
+    removeMarkers();
+    data.forEach(function (service) {
+      addMarker(service);
+    });
+  }
+
+  /**
+   * Adds a marker to the map with the attributes needed.
+   * @param service - The service object to add to the map.
+   */
+  function addMarker(service) {
+    var name = service.name;
+    var lat = service.locations[0].latitude;
+    var lng = service.locations[0].longitude;
+    var desc = service.locations[0].description;
+    var city = service.locations[0].address_attributes.city;
+    var state = service.locations[0].address_attributes.state;
+    var street = service.locations[0].address_attributes.street;
+    var zip = service.locations[0].address_attributes.zip;
+    var hours = service.locations[0].hours;
+    var urls = service.locations[0].urls;
+
+    var message = '<h4>' + name + '<h4>\n<h5>' + desc + '</h5>\n<h5>' + hours + '</h5>';
+
+    $scope.markers.push({
+        lat: lat,
+        lng: lng,
+        message: message
+    });
+  };
+
+  /**
+   * Removes all markers from the array by creating a new one.
+   */
+  function removeMarkers() {
+      $scope.markers = [];
+  }
 };
--- test/client_unit/controllers/test.map-controller.js
+++ test/client_unit/controllers/test.map-controller.js
@@ -1,34 +1,87 @@
 var expect = chai.expect;
+var scope, search, leafletData, ctrl;
 
 describe('MapController', function(done) {
 
-  var scope, ctrl;
-
-  // Setup: include benefisher module;
-  beforeEach(module('benefisher'));
+  /** SETUP **/
+  // Mock controller dependencies.
+  beforeEach(createDependencyMocks);
+  // Set up the controller object
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('MapController', { $scope:scope, search: search, leafletData: leafletData });
+  }))
 
   // Defaults
-  it('should create map defaults', inject(function($controller) {
-    scope = {};
-    ctrl = $controller('MapController', {$scope:scope});
+  it('should create map defaults', function() {
     // Defaults should be an object
     expect(scope.defaults).to.be.an('object');
     // Defaults must contain the tileLayer property.
     expect(scope.defaults.tileLayer).to.be.a('string');
     expect(scope.defaults.tileLayer.length).to.be.above(0);
-  }));
+  });
 
+  /** TESTS **/
   // Center
-  it ('should create map center', inject(function($controller) {
-    scope = {};
-    ctrl = $controller('MapController', {$scope:scope});
+  it ('should create map center', function() {
     // Center should be an object
     expect(scope.center).to.be.an('object');
     // Center must have lat and lng.
     expect(scope.center.lat).to.be.a('number');
     expect(scope.center.lng).to.be.a('number');
-  }));
+  });
 
   // Markers
   // Controls
+
+  // Events
+  it('should call search on map move event', function() {
+    scope.fireEvent('leafletDirectiveMap.moveend', {});
+    expect(search.search).to.have.been.calledWith({ bounds: '39,-122,37,-120' });
+  });
 });
+
+function createDependencyMocks()
+{
+  // Mock scope dependency
+  scope = {
+    events: {},
+    $on: function(eventName, callback) {
+      this.events[eventName] = callback;
+    },
+    fireEvent: function(eventName, event) {
+      this.events[eventName](event);
+    }
+  };
+  // Mock search dependency
+  search = {
+    search: sinon.spy()
+  };
+  // Mock leafletData dependency
+  var bounds = {
+    getNorthWest: function() {
+      return {
+        lat: 39,
+        lng: -122
+      }
+    },
+    getSouthEast: function() {
+      return {
+        lat: 37,
+        lng: -120
+      }
+    }
+  };
+  var map = {
+    getBounds: function() {return bounds; }
+  };
+  var getMapPromise = {
+    then: function(callback) {
+      callback(map);
+    }
+  };
+  leafletData = {
+    getMap: function() {
+      return getMapPromise;
+    }
+  };
+}
