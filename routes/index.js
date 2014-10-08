4cbd642d75c255c2c2cae81b4d6f0b0e59392549
Jesse Rosato <jesse.rosato@me.com>
2014-10-08 15:52:30 -0700

    Added dotenv environment variable manager.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+// Load environment variables
+var dotenv = require('dotenv');
+dotenv.load();
 
 /* GET home page. */
 router.get('/', function(req, res) {
