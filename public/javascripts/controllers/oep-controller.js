9a0deb69125f023c9a5430496bbd25615284ff65
James Doan <jamesdoan@csus.edu>
2014-10-10 16:51:54 -0700

    added files

--- public/javascripts/controllers/oep-controller.js
+++ public/javascripts/controllers/oep-controller.js
@@ -6,4 +6,9 @@ var OepController = function ($scope, $http) {
   $http.get('oepterms/oep.json').success(function(data) {
     $scope.oepterms = data;
   });
+
+  $scope.doFilter = function () {
+    $scope.filterResult = $scope.search;
+  }
+
 }
