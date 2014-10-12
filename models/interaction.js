b9cc73cb9f6d766517e93e7970e2f4772c336e5e
Jesse Rosato <jesse.rosato@me.com>
2014-10-12 14:57:15 -0700

    Added interaction model.

---
+++ models/interaction.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * Create Interaction model.
+ * @param sequelize
+ * @param DataTypes
+ * @returns {*}
+ */
+module.exports = function(sequelize, DataTypes) {
+  var Interaction = sequelize.define("Interaction", {
+    target: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Interaction.hasOne(models.Result)
+      }
+    }
+  });
+
+  return Interaction;
+
+};
