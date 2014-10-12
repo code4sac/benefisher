0969d1d406a3b66b19e776cf7e90b5a8ca671e41
Jesse Rosato <jesse.rosato@me.com>
2014-10-11 20:55:24 -0700

    Added sequelize ORM.

--- models/result.js
+++ models/result.js
@@ -1,123 +1,156 @@
-/**
- * Created by jesserosato on 10/9/14.
- */
+"use strict";
 
-/**
- * Location model
- * @param data
- * @returns {Array}
- * @constructor
- */
-var Result = function(data) {
-
-  var result = {};
-
-  // Make sure we have data to work with.
-  if ( ! data) {
-    return result;
-  }
-
-  mungeData(data);
-
-  return result;
-
-  /**
-   * Munge data from API into usable model.
-   * @param service
-   */
-  function mungeData(location) {
-    var phone = formatPhone(location);
-    var phoneUrl = formatPhoneUrl(location);
-    var email = location.emails ? location.emails[0] : null;
-    var address = formatAddress(location);
-    var latitude = location.coordinates ? location.coordinates[1] : null;
-    var longitude = location.coordinates ? location.coordinates[0] : null;
-    result = {
-      name: location.name,
-      lat: latitude,
-      lng: longitude,
-      description: location.description,
-      address: address,
-      directionsUrl: 'http://maps.google.com/maps?saddr=Current+Location&daddr=' + encodeURIComponent(address),
-      hours: location.hours,
-      phone: phone,
-      phoneUrl: phoneUrl,
-      email: email,
-      emailurl: 'mailto:' + email,
-      url: location.urls
-    };
-    result.popup = generatePopupHtml(result);
-  }
-
-  /**
-   * Format a location's street address.
-   * @param location
-   * @returns {string}
-   */
-  function formatAddress(location)
-  {
-    var city = location.address.city;
-    var state = location.address.state;
-    var street = location.address.street;
-    var zip = location.address.zip;
-    return street + ', ' + city + ', ' + state + ' ' + zip;
-  }
-
-  /**
-   * Format a telephone number
-   * @param location
-   * @returns {string}
-   */
-  function formatPhone(location)
-  {
-    var phones = getPhones(location);
-    if ( ! phones) {
-      return null;
+module.exports = function(sequelize, DataTypes) {
+  var Result = sequelize.define("Result", {
+    name: DataTypes.STRING,
+    lat: DataTypes.FLOAT,
+    lng: DataTypes.FLOAT,
+    description: DataTypes.TEXT,
+    address: DataTypes.STRING,
+    hours: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    rawPhone: DataTypes.STRING,
+    email: DataTypes.STRING,
+    emailurl: DataTypes.STRING,
+    url: DataTypes.STRING
+  }, {
+    getterMethods: {
+      directionsUrl: function() {
+        return formatDirectionsUrl(this.getDataValue('address'));
+      },
+      phoneUrl: function() {
+        return formatPhoneUrl(this.getDataValue('rawPhone'));
+      },
+      emailUrl: function() {
+        return formatEmailUrl(this.getDataValue('email'));
+      }
+    },
+    instanceMethods: {
+      toJSON: function () {
+        var json = this.values
+        json.directionsUrl = formatDirectionsUrl(this.getDataValue('address'));
+        json.phoneUrl = formatPhoneUrl(this.getDataValue('rawPhone'));
+        json.emailUrl = formatEmailUrl(this.getDataValue('email'));
+        json.popup = generatePopupHtml(this.getDataValue('name'), this.getDataValue('hours'));
+        return json;
+      },
+      setLocation: function(location) {
+        var email = location.emails ? location.emails[0] : null;
+        this.setDataValue('name', location.name);
+        this.setDataValue('lat', location.coordinates ? location.coordinates[1] : null);
+        this.setDataValue('lng', location.coordinates ? location.coordinates[0] : null);
+        this.setDataValue('description', location.description);
+        this.setDataValue('address', formatAddress(location));
+        this.setDataValue('hours', location.hours);
+        this.setDataValue('phone', formatPhone(location));
+        this.setDataValue('rawPhone', formatRawPhone(location));
+        this.setDataValue('email', email);
+        this.setDataValue('url', location.urls);
+        return this;
+      }
     }
-    var rawPhone = phones.number;
-    var extension = phones.extension;
-    // For simplicity, only attempt to format phone numbers that are just 10 plain digits.
-    if (rawPhone.match(/^[0-9]{10}$/)) {
-      var phone = '(' + rawPhone.substr(0, 3) + ') ' + rawPhone.substr(3,3) + '-' + rawPhone.substr(6,4);
-    }
-    return (phone ? phone : rawPhone) + (extension ? ' ' + extension : '');
-  }
+  });
 
-  /**
-   * Format a telephone URL for mobile devices.
-   * @param location
-   * @returns {string}
-   */
-  function formatPhoneUrl(location)
-  {
-    var phones = getPhones(location);
-    if ( ! phones) {
-      return null;
-    }
-    var rawPhone = phones.number;
-    var extension = phones.extension;
-    return phoneUrl = 'tel: ' + rawPhone + (extension ? ',' + extension.replace(/[^0-9]/g, '') : '');
-  }
-
-  /**
-   * Get phones array from location
-   * @param location
-   * @returns {location.phones|*}
-   */
-  function getPhones(location)
-  {
-    return location.phones ? location.phones[0] : (location.phones_attributes ? location.phones_attributes[0] : false);
-  }
-
-  /**
-   * Generate the HTML for a popup for a result.
-   * TODO: Use a jade template for this.
-   * @param result
-   */
-  function generatePopupHtml(result) {
-    return '<h4>' + result.name + '</h4><h5>' + result.hours + '</h5>';
-  }
-
+  return Result;
 };
 
-module.exports = Result;
+/**
+ * Format a location's street address.
+ * @param location
+ * @returns {string}
+ */
+function formatAddress(location)
+{
+  var city = location.address.city;
+  var state = location.address.state;
+  var street = location.address.street;
+  var zip = location.address.zip;
+  return street + ', ' + city + ', ' + state + ' ' + zip;
+}
+
+/**
+ * Format a Google maps directions URL
+ * @param address
+ * @returns {string}
+ */
+function formatDirectionsUrl(address)
+{
+  return 'http://maps.google.com/maps?saddr=Current+Location&daddr=' + encodeURIComponent(address);
+}
+
+/**
+ * Format a telephone number
+ * @param location
+ * @returns {string}
+ */
+function formatPhone(location)
+{
+  var phones = getPhones(location);
+  if ( ! phones) {
+    return null;
+  }
+  var rawPhone = phones.number;
+  var extension = phones.extension;
+  // For simplicity, only attempt to format phone numbers that are just 10 plain digits.
+  if (rawPhone.match(/^[0-9]{10}$/)) {
+    var phone = '(' + rawPhone.substr(0, 3) + ') ' + rawPhone.substr(3,3) + '-' + rawPhone.substr(6,4);
+  }
+  return (phone ? phone : rawPhone) + (extension ? ' ' + extension : '');
+}
+
+/**
+ * Format a phone number for use in a link.
+ * @param location
+ * @returns {*}
+ */
+function formatRawPhone(location)
+{
+  var phones = getPhones(location);
+  if ( ! phones) {
+    return null;
+  }
+  var rawPhone = phones.number;
+  var extension = phones.extension;
+  return rawPhone + (extension ? ',' + extension.replace(/[^0-9]/g, '') : '');
+}
+
+/**
+ * Format a phone url (for smartphone browsers)
+ * @param rawPhone
+ */
+function formatPhoneUrl(rawPhone)
+{
+  return 'tel: ' + rawPhone;
+}
+
+/**
+ * Format an email url
+ * @param email
+ * @returns {string}
+ */
+function formatEmailUrl(email)
+{
+  return 'mailto:' + email;
+}
+
+/**
+ * Get phones array from location
+ * @param location
+ * @returns {location.phones|*}
+ */
+function getPhones(location)
+{
+  return location.phones ? location.phones[0] : (location.phones_attributes ? location.phones_attributes[0] : false);
+}
+
+/**
+ * Generate the HTML for a popup for a result.
+ * TODO: Use a jade template for this.
+ * @param name
+ * @param hours
+ * @returns {string}
+ */
+function generatePopupHtml(name, hours)
+{
+  return '<h4>' + name + '</h4><h5>' + hours + '</h5>';
+}
