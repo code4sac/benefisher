f0ca6ce3ef0aa53e9177cc943609adf582061cf6
Jesse Rosato <jesse.rosato@me.com>
2014-10-11 23:27:21 -0700

    Added comments to Query model.

--- models/query.js
+++ models/query.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/**
+ * Create Query model.
+ * @param sequelize
+ * @param DataTypes
+ * @returns {*}
+ */
 module.exports = function(sequelize, DataTypes) {
   var Query = sequelize.define("Query", {
     bounds: DataTypes.STRING,
