7b6a629970f993c37674d1b33de2fc0e87cabb81
Jesse Rosato <jesse.rosato@me.com>
2014-10-12 15:01:58 -0700

    Changed stats route to interactions route.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var search = require('./routes/search');
-var stats = require('./routes/stats');
+var interactions = require('./routes/interactions');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/search', search);
-app.use('/stats', stats);
+app.use('/interactions', interactions);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- test/server_integration/test.route.interactions.js
+++ test/server_integration/test.route.interactions.js
@@ -3,11 +3,11 @@ var request = require('supertest');
 // Here we get hold of the express application
 var app = require("../../app.js");
 
-describe('POST /stats', function() {
+describe('POST /interactions', function() {
 
   it('should respond with json and 201 when good data is provided', function(done){
     request(app) // the request-object is the supertest top level api
-      .post('/stats')
+      .post('/interactions')
       .send({ stat: { data: "test" }})
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -16,7 +16,7 @@ describe('POST /stats', function() {
 
   it('should throw 400 when no data is provided', function(done) {
     request(app) // the request-object is the supertest top level api
-      .post('/stats')
+      .post('/interactions')
       .send({ type: 'query', data: {} })
       .set('Accept', 'application/json')
       .expect(400, done); // note that we're passing the done as parameter to the expect
