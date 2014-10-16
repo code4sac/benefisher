521a6551090413e0ee141b8719e2d8b82b35c814
Jesse Rosato <jesse.rosato@me.com>
2014-10-16 15:07:43 -0700

    Debugging Travis issues.

--- models/index.js
+++ models/index.js
@@ -10,6 +10,7 @@ dotenv.load();
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
 config = configure(config);
+console.log(config.host);
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
