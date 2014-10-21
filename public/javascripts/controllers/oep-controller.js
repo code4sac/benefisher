7b47d09e1f27bca2ba48359f1f14af6b7e05a472
James Doan <jamesdoan@csus.edu>
2014-10-20 17:53:12 -0700

    Testing a different tags input module

--- public/javascripts/controllers/oep-controller.js
+++ public/javascripts/controllers/oep-controller.js
@@ -1,14 +1,45 @@
 /**
  * Created by jamesdoan on 10/9/14.
  */
+ var OepController = function($scope, $http, $timeout) {
 
-var OepController = function ($scope, $http) {
   $http.get('oepterms/oep.json').success(function(data) {
     $scope.oepterms = data;
   });
 
-  $scope.doFilter = function () {
-    $scope.filterResult = $scope.search;
+  $scope.disabled = undefined;
+  $scope.searchEnabled = undefined;
+
+  $scope.enable = function() {
+    $scope.disabled = false;
+  };
+
+  $scope.disable = function() {
+    $scope.disabled = true;
+  };
+
+  $scope.enableSearch = function() {
+    $scope.searchEnabled = true;
   }
 
-}
+  $scope.disableSearch = function() {
+    $scope.searchEnabled = false;
+  }
+
+  $scope.someGroupFn = function (item){
+
+    if (item.name[0] >= 'A' && item.name[0] <= 'M')
+      return 'From A - M';
+
+    if (item.name[0] >= 'N' && item.name[0] <= 'Z')
+      return 'From N - Z';
+
+  };
+
+  $scope.counter = 0;
+  $scope.someFunction = function (item, model){
+    $scope.counter++;
+    $scope.eventResult = {item: item, model: model};
+  };
+  $scope.oepterms = [];
+};
