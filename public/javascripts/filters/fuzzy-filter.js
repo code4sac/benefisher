2067cc6b9a1822c4aca76bc3291f74eb0254a7cb
Jesse Rosato <jesse.rosato@me.com>
2014-11-27 16:41:03 -0800

    Fix issue with search autocomplete throwing errors

--- public/javascripts/filters/fuzzy-filter.js
+++ public/javascripts/filters/fuzzy-filter.js
@@ -1,15 +1,19 @@
+var noResult = { name: "Sorry, no results found!", oe_id: 99999, disabled: true }
+
 angular.module('benefisher.filters', []).filter('fuzzy', function() {
   return function(terms, enteredText) {
     var filtered = [];
+    // Credit to Dustin Diaz for this regex: http://www.dustindiaz.com/autocomplete-fuzzy-matching
     var reg = new RegExp(enteredText.split('').join('\\w*').replace(/\W/, ""), 'i');
     var i = terms.length;
     while (i) {
-      if (terms[--i].name.match(reg)) {
-        filtered.push(terms[i]);
+      var term = terms[--i];
+      if (term.name.match(reg)) {
+        filtered.push(term);
       }
     }
     if ( ! filtered.length) {
-      filtered.push({ name: "Sorry, nothing found matching " + enteredText + ".", disabled: true });
+      filtered = [noResult];
     }
     return filtered;
   };
