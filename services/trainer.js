75aa1fe9f217f33081092014641c41a1fb9a9449
Anthony Cristiano <cristiano@csus.edu>
2014-12-01 22:54:57 -0800

    Create cron job for training

---
+++ services/trainer.js
@@ -0,0 +1,9 @@
+
+
+var Trainer = {
+  performJob : function (name) {
+    //console.log("Hello, " + name + ". Your cron job is working.");
+  }
+};
+
+module.exports = Trainer;
---
+++ test/server_unit/test.service.trainer.js
@@ -0,0 +1,3 @@
+/**
+ * Created by anthony on 12/1/14.
+ */
